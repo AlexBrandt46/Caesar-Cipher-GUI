db484dfab2af209e553da9a11038c476a6eac844
AlexBrandt46 <ajb3155@gmail.com>
2017-11-05 12:43:00 -0500

    Initial Commit. Includes GUI Design

---
+++ Caesar Cipher GUI/Caesar Cipher GUI/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Caesar_Cipher_GUI {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtDecypted = new System.Windows.Forms.TextBox();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDecypted
+            // 
+            this.txtDecypted.Location = new System.Drawing.Point(13, 13);
+            this.txtDecypted.Name = "txtDecypted";
+            this.txtDecypted.Size = new System.Drawing.Size(530, 20);
+            this.txtDecypted.TabIndex = 0;
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Location = new System.Drawing.Point(13, 218);
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(530, 20);
+            this.txtEncrypted.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnEncrypt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEncrypt.Location = new System.Drawing.Point(96, 98);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 48);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDecrypt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDecrypt.Location = new System.Drawing.Point(378, 98);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 47);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(555, 251);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.txtDecypted);
+            this.Name = "Form1";
+            this.Text = "Caesar Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDecypted;
+        private System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
